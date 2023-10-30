f652aeb65080c6b618e979154aa3948621783b5f
ErgothDK <ergothdk@gmail.com>
2023-10-30 17:01:54 -0600

    Added functionality to counter buttons

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,28 @@
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const counterButtonHandler = (event) => {
+    dispatch({ type: event.target.name.toUpperCase() });
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={counterButtonHandler} name="decrement">
+          -
+        </button>
+        <button onClick={counterButtonHandler} name="increment">
+          +
+        </button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
